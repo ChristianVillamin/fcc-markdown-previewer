6fba8e9263a5e81c48c7922651b041a1fe6d23bb
Christian Villamin <ChristianVillamin31@gmail.com>
2019-06-30 19:02:30 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import marked from 'marked';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const MarkdownPreviewer = () => {
+  const [input, setInput] = React.useState(`
+# Heading One
+## This is heading number two!
+### Link
+[freeCodeCamp](https://freecodecamp.org)
+### Inline Code
+\`console.log('Nice')\`
+### Code Block
+\`\`\`
+  npm install
+\`\`\`
+### List
+1. Item 1
+1. Item 2
+### Quote
+>This is a block quote.
+### Image
+![Markdown Logo](https://markdown-here.com/img/icon32.png)
+### Bold Text
+**Bolded**`);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  const handleChange = event => setInput(event.target.value);
+
+  return (
+    <>
+      <h1 id="title">Markdown Previewer</h1>
+
+      <div id="container">
+        <div id="input">
+          <h2 className="centeroo">Input</h2>
+          <hr /> <br />
+          <textarea id="editor" onChange={handleChange}>
+            {input}
+          </textarea>
+        </div>
+
+        <div id="output">
+          <h2 className="centeroo">Output</h2>
+          <hr /> <br />
+          <div
+            id="preview"
+            dangerouslySetInnerHTML={{ __html: marked(input) }}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+marked.setOptions({ breaks: true });
+ReactDOM.render(<MarkdownPreviewer />, document.getElementById('root'));
